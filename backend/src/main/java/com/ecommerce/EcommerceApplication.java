4e690aa7018c69120039d2d55d3c9c4e639125b6
Mugilvannan P <mugilvannanpichai02@gmail.com>
2026-06-13 15:08:25 +0530

    add .env

--- backend/src/main/java/com/ecommerce/EcommerceApplication.java
+++ backend/src/main/java/com/ecommerce/EcommerceApplication.java
@@ -7,11 +7,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("PORT", dotenv.get("PORT"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
